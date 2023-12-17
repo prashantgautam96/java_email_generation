f71aff40b1663b483892929926400ea5085c653b
prashantgautam96 <2020ucs0114@iitjammu.ac.in>
2023-12-17 18:33:56 +0530

    This is the email creation logic for any new person comming to the institution we only have to give the firstName and lastname as input and it will generate repective email and password with that.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import emailapp.Email;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Email email = new Email("Prashant","Gautam");
+        email.setAlternateEmail("infinitiansgautam@gmail.com");
+        email.getInfo();
+    }
+}
